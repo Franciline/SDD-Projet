45a59edc00d1be9cd0eb31149dbd892d0118d80b
FlyFird <92992243+FlyFird@users.noreply.github.com>
2024-02-28 15:27:36 +0100

    Update Chaine.c

--- Ressources/Ressources/Chaine.c
+++ Ressources/Ressources/Chaine.c
@@ -5,6 +5,71 @@
 
 // Exercice 1
 
-Chaines* lectureChaine(FILE *f) {
+Chaines* creer_chaine() {
+    Chaines* chaine = (Chaines*) malloc(sizeof(Chaines));
+    chaine->gamma = 0;
+    chaine->nbChaines = 0;
+    chaine->chaines = NULL;
+}
 
+
+Chaines* lectureChaine(FILE *f) {
+    FILE* fichier = fopen(f, "r");
+
+    if (fichier == NULL) {
+        printf("Erreur lors du chargement du fichier %s\n", f);
+        exit(1);
+    }
+
+    char nbChain_s[10];
+    int nbChain_i;
+
+    char gamma_s[10];
+    int gamma_i;
+
+    int num;
+    int nb_pts;
+    double x;
+    double y;
+
+    Chaines* chaine = creer_chaine();
+
+    CellChaine* cellC_suiv;
+    CellPoint* cellP_suiv;
+
+    int TAILLE_MAX = 20
+    char ligne[TAILLE_MAX];
+
+    // On recupere la 1ere ligne du fichier qui contient nbChain
+    fgets(ligne, sizeof(ligne), fichier);
+    sscanf(ligne, "%ms %d", &nbChain_s, &nbChain_i);
+    
+    // On recupere la 2eme ligne du fichier qui contient gamma
+    fgets(ligne, sizeof(ligne), fichier);
+    sscanf(ligne, "%ms %d", &gamma_s, &gamma_i);
+
+    while(fgets(ligne, TAILLE_MAX, fichier) != NULL) {
+        fscanf(fichier, "%d %d", &num, &nb_pts);
+        //sscanf(ligne, "%d %d %f", num, nb_pts, liste_points);
+        
+        CellChaine* cellC = (CellChaine*) malloc(sizeof(CellChaine));
+        cellC->numero = num;
+        chaine->chaines = cellC;
+
+        CellPoint* cellP = (CellPoint*) malloc(sizeof(CellPoint));
+
+        for (int i = 0; i<nb_pts; i++) {
+            fscanf(fichier, "%f %f", &x, &y);
+            CellPoint* cellP = (CellPoint*) malloc(sizeof(CellPoint));
+            cellP->x = x;
+            cellP->y = y;
+            cellP->suiv = cellC->points;
+            cellC->points = cellP;
+        }
+
+        cellC->suiv = chaine->chaines;
+        chaine->chaines = CellC;
+    }
+    
+    return chaine;
 }
