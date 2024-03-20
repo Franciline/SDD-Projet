4fc1adf69c4b45a96e32839d27d003460ab11d32
franciline <ameliechu@outlook.com>
2024-03-20 13:43:04 +0100

    Suppression de enonce.pdf et Question 1 EXO 4 (Hachage.h)

--- Ressources/Ressources/Hachage.h
+++ Ressources/Ressources/Hachage.h
@@ -1,5 +1,12 @@
+#ifndef HACHAGE_H
+#define HACHAGE_H
+
+#include "Reseau.h"
+
 typedef struct{
   int nbElement; //pas necessaire ici
   int tailleMax;
   CellNoeud** T;
 } TableHachage ;
+
+#endif
