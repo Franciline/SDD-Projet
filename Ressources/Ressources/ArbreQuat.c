01df83f08b326efd3a5126418f403e76e5f2bbbb
FlyFird <92992243+FlyFird@users.noreply.github.com>
2024-03-27 02:34:11 +0100

    Update ArbreQuat.c

--- Ressources/Ressources/ArbreQuat.c
+++ Ressources/Ressources/ArbreQuat.c
@@ -39,8 +39,102 @@ void chaineCoordMinMax(Chaines* C, double* xmin, double* ymin, double* xmax, dou
     *xmin = xmi; *ymin = ymi; *xmax = xma; *ymax = yma;
 }
 
+//Question 2
+/*Fonction qui creer une cellule de l'arbre quaternaire*/
+ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY) {
+    ArbreQuat* new_aq = (ArbreQuat*) malloc(sizeof(ArbreQuat));
+
+    new_aq->xc = xc;
+    new_aq->yc = yc;
+    new_aq->coteX = coteX;
+    new_aq->coteY = coteY;
+    new_aq->noeud = creer_noeud();
+    new_aq->so = NULL;
+    new_aq->se = NULL;
+    new_aq->no = NULL;
+    new_aq->ne = NULL;
+
+    return new_aq;
+}
+
+
 //Question 3
-/*INsere un NOeud du réseau dans un arbre*/
+/*Insere un Noeud du reseau dans un arbre*/
 void insererNoeudArbre(Noeud* n, ArbreQuat** a, ArbreQuat* parent){
     //
+}
+
+//Question 4
+/*Retourne le noeud de R correspondant, sinon cree le noeud et l'ajoute dans R*/
+Noeud* rechercheCreeNoeudArbre(Reseau* R, ArbreQuat** a, ArbreQuat* parent, double x, double y) {
+    ArbreQuat* arbre = *a;
+
+    //Cas arbre vide
+    if (arbre == NULL) {
+        //on cree un nouveau noeud
+        Noeud* new_noeud = creer_noeud();
+        new_noeud->x = x;
+        new_noeud->y = y;
+        new_noeud->num = R->nbNoeuds + 1;
+
+        //creation de la cellule pour R
+        CellNoeud* new_cellnR = (CellNoeud*)(malloc(sizeof(CellNoeud)));
+        new_cellnR->nd = new_noeud; //on affecte le noeud dans la cellule
+        new_cellnR->suiv = R->noeuds; //on ajoute à la suite de la liste de CellNoeud dans Reseau
+        
+        //ajout dans R
+        R->noeuds = new_cellnR;
+        R->nbNoeuds = R->nbNoeuds + 1; //on incrémente le nombre de noeuds
+
+        //ajout dans a
+        insererNoeudArbre(new_noeud, a, parent);
+
+        return new_noeud;
+    }
+
+    //Cas feuille
+    if (arbre->noeud != NULL) {
+        if ((x == arbre->noeud->x) && (y == arbre->noeud->y)) {
+            return arbre->noeud;
+        }
+        else {
+            //on cree un nouveau noeud
+            Noeud* new_noeud = creer_noeud();
+            new_noeud->x = x;
+            new_noeud->y = y;
+            new_noeud->num = R->nbNoeuds + 1;
+
+            //creation de la cellule pour R
+            CellNoeud* new_cellnR = (CellNoeud*)(malloc(sizeof(CellNoeud)));
+            new_cellnR->nd = new_noeud; //on affecte le noeud dans la cellule
+            new_cellnR->suiv = R->noeuds; //on ajoute à la suite de la liste de CellNoeud dans Reseau
+            
+            //ajout dans R
+            R->noeuds = new_cellnR;
+            R->nbNoeuds = R->nbNoeuds + 1; //on incrémente le nombre de noeuds
+
+            //ajout dans a
+            insererNoeudArbre(new_noeud, a, parent);
+
+            return new_noeud;
+        }
+    }
+
+    //Cas cellule interne
+    if ((arbre != NULL) && (arbre->noeud == NULL)) {
+        if (x < parent->xc) && (y < parent->yc) {
+            rechercheCreeNoeudArbre(R, arbre, parent->so, x, y);
+        }
+        if (x > parent->xc) && (y > parent->yc) {
+            rechercheCreeNoeudArbre(R, arbre, parent->ne, x, y);
+        }
+        if (x < parent->xc) && (y > parent->yc) {
+            rechercheCreeNoeudArbre(R, arbre, parent->no, x, y);
+        }
+        if (x > parent->xc) && (y < parent->yc) {
+            rechercheCreeNoeudArbre(R, arbre, parent->se, x, y);
+        }
+    }
+
+    return new_noeud;
 }
