bc0588ea0f58376e0ee577caf8bf0d7df61522ea
FlyFird <92992243+FlyFird@users.noreply.github.com>
2024-04-24 14:42:55 +0200

    Update ArbreQuat.h

--- Ressources/Ressources/ArbreQuat.h
+++ Ressources/Ressources/ArbreQuat.h
@@ -1,6 +1,8 @@
 #ifndef __ARBRE_QUAT_H__
 #define __ARBRE_QUAT_H__
+
 #include "Reseau.h"
+
 
 /* Arbre quaternaire contenant les noeuds du reseau */
 typedef struct arbreQuat{
@@ -8,19 +10,20 @@ typedef struct arbreQuat{
     double coteX;           /* Longueur de la cellule */
     double coteY;           /* Hauteur de la cellule */
     Noeud* noeud;           /* Pointeur vers le noeud du reseau */
-    struct arbreQuat *so;   /* Sous-arbre sud-ouest, pour x < xc et y < yc */
-    struct arbreQuat *se;   /* Sous-arbre sud-est, pour x >= xc et y < yc */
-    struct arbreQuat *no;   /* Sous-arbre nord-ouest, pour x < xc et y >= yc */
-    struct arbreQuat *ne;   /* Sous-arbre nord-est, pour x >= xc et y >= yc */
+    struct arbreQuat* so;   /* Sous-arbre sud-ouest, pour x < xc et y < yc */
+    struct arbreQuat* se;   /* Sous-arbre sud-est, pour x >= xc et y < yc */
+    struct arbreQuat* no;   /* Sous-arbre nord-ouest, pour x < xc et y >= yc */
+    struct arbreQuat* ne;   /* Sous-arbre nord-est, pour x >= xc et y >= yc */
 } ArbreQuat;
 
-void chaineCoordMinMax(Chaines* C, double* xmin, double* ymin, double* xmax, double* ymax);
-ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY);
-void insererNoeudArbre(Noeud* n, ArbreQuat** a, ArbreQuat* parent);
-Noeud* rechercheCreeNoeudArbre(Reseau* R, ArbreQuat** a, ArbreQuat* parent, double x, double y);
-Reseau* reconstitueReseauArbre(Chaines* C);
 
-//Desallocation
+void chaineCoordMinMax(Chaines* C, double* xmin, double* ymin, double* xmax, double* ymax);         // Exercice 5 Question 1
+ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY);                        // Exercice 5 Question 2
+void insererNoeudArbre(Noeud* n, ArbreQuat** a, ArbreQuat* parent);                                 // Exercice 5 Question 3
+Noeud* rechercheCreeNoeudArbre(Reseau* R, ArbreQuat** a, ArbreQuat* parent, double x, double y);    // Exercice 5 Question 4
+Reseau* reconstitueReseauArbre(Chaines* C);                                                         // Exercice 5 Question 5
+
+//Fonctions de desallocation
 void liberer_arbre(ArbreQuat* a);
 
 #endif
