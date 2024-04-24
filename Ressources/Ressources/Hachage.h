48548eed10a68fef92aeab1a22e79a3ce8be6777
FlyFird <92992243+FlyFird@users.noreply.github.com>
2024-04-24 15:36:00 +0200

    Update Hachage.h

--- Ressources/Ressources/Hachage.h
+++ Ressources/Ressources/Hachage.h
@@ -1,21 +1,25 @@
 #ifndef HACHAGE_H
 #define HACHAGE_H
+
 #include "Reseau.h"
+
 
 typedef struct{
   int nbElement; //pas necessaire ici
   int tailleMax;
-  CellNoeud ** T;
+  CellNoeud** T;
 } TableHachage ;
 
-int h(TableHachage* t, int k);
-int f(int x, int y);
 
-Reseau* reconstitueReseauHachage(Chaines *C, int M);
-Noeud* rechercheCreeNoeudHachage(Reseau* R, TableHachage* H, double x, double y);
+int f(int x, int y);                                                                // Exercice 4 Question 2
+int h(TableHachage* t, int k);                                                      // Exercice 4 Question 3
+Noeud* rechercheCreeNoeudHachage(Reseau* R, TableHachage* H, double x, double y);   // Exercice 4 Question 4
+Reseau* reconstitueReseauHachage(Chaines* C, int M);                                // Exercice 4 Question 5
+
+//Fonctions de base
 TableHachage* initTableHachage(int taille);
 
-//Desallocation
+//Fonction de desallocation
 void liberer_tablehachage(TableHachage* t);
 
 #endif
