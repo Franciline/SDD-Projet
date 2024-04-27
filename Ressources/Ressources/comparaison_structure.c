9007f75e45e1201601d2cc36b4bba9d3f87ff1b0
franciline <ameliechu@outlook.com>
2024-04-27 10:20:32 +0200

    Travail sur les instances

--- Ressources/Ressources/comparaison_structure.c
+++ Ressources/Ressources/comparaison_structure.c
@@ -16,42 +16,43 @@ double temps_cpu_liste, temps_cpu_hachage, temps_cpu_arbre;
 int main(int argc, char** argv){
 
     //ouverture du fichier de chaines
-    FILE * fichier1 = fopen("00014_burma.cha", "r");
-    FILE * fichier2 = fopen("comparaison.txt", "w");
-    Chaines* test = lectureChaines(fichier1);
+    FILE * source = fopen("08000_USA-road-d-NY.cha", "r");
+    FILE * destination = fopen("instance_comparaison.txt", "a");
+    Chaines* chaines = lectureChaines(source);
     int taille_tab = 20;
     Reseau* test_reseau = NULL;
 
-    fprintf(fichier2, "Temps de calcul pour\n\n");
+    //fprintf(destination, "nbChaine ABQ LC H50 H100 H150 H200 H250 H300 H350 H400 H450 H500 \n\n");
+    fprintf(destination, "%d ",chaines->nbChaines);
 
     //Arbre quaternaire
     temps_initial = clock();
-    test_reseau = reconstitueReseauArbre(test);
+    test_reseau = reconstitueReseauArbre(chaines);
     temps_final = clock();
     temps_cpu_arbre = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
-    fprintf(fichier2, "Arbre Quaternaire \n%lf\n",temps_cpu_arbre);
-    liberer_reseau(test_reseau);
+    fprintf(destination, "%lf ",temps_cpu_arbre);
+    //liberer_reseau(test_reseau);
 
     //Liste chainée
     temps_initial = clock();
-    test_reseau = reconstitueReseauListe(test);
+    test_reseau = reconstitueReseauListe(chaines);
     temps_final = clock();
     temps_cpu_liste = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
-    fprintf(fichier2, "Liste chainee\n%lf\n",temps_cpu_liste);
-    liberer_reseau(test_reseau);
+    fprintf(destination, "%lf ",temps_cpu_liste);
+    //liberer_reseau(test_reseau);
 
     //Table de hachage
-    for (taille_tab = 10; taille_tab <= 500; taille_tab+=50){
+    for (taille_tab = 50; taille_tab <= 501; taille_tab+=50){
         temps_initial = clock();
-        test_reseau = reconstitueReseauHachage(test, taille_tab);
+        test_reseau = reconstitueReseauHachage(chaines, taille_tab);
         temps_final = clock();
         temps_cpu_hachage = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
-        fprintf(fichier2, "Table de hachage taille = %d\n%lf\n",taille_tab, temps_cpu_hachage);
+        fprintf(destination, "%lf ", temps_cpu_hachage);
         liberer_reseau(test_reseau);
     }
-
-    liberer_chaine(test);
-    fclose(fichier1);
-    fclose(fichier2);
+    fprintf(destination,"\n");
+    liberer_chaine(chaines);
+    fclose(source);
+    fclose(destination);
 
 }
