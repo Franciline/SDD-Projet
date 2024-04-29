95d0868df9aca8582eb40943cbd52609dcf75392
franciline <ameliechu@outlook.com>
2024-04-29 07:10:10 +0200

    Derniere instance

--- Ressources/Ressources/comparaison_structure.c
+++ Ressources/Ressources/comparaison_structure.c
@@ -16,11 +16,11 @@ double temps_cpu_liste, temps_cpu_hachage, temps_cpu_arbre;
 int main(int argc, char** argv){
 
     //ouverture du fichier de chaines
-    FILE * source = fopen("08000_USA-road-d-NY.cha", "r");
+    FILE * source = fopen("10000_USA-road-d-NY.cha", "r");
     FILE * destination = fopen("instance_comparaison.txt", "a");
     Chaines* chaines = lectureChaines(source);
     int taille_tab = 20;
-    Reseau* test_reseau = NULL;
+    Reseau* test_reseau;
 
     //fprintf(destination, "nbChaine ABQ LC H50 H100 H150 H200 H250 H300 H350 H400 H450 H500 \n\n");
     fprintf(destination, "%d ",chaines->nbChaines);
@@ -32,7 +32,7 @@ int main(int argc, char** argv){
     temps_cpu_arbre = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
     fprintf(destination, "%lf ",temps_cpu_arbre);
     //liberer_reseau(test_reseau);
-
+    printf("arbre fait\n");
     //Liste chainée
     temps_initial = clock();
     test_reseau = reconstitueReseauListe(chaines);
@@ -40,7 +40,7 @@ int main(int argc, char** argv){
     temps_cpu_liste = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
     fprintf(destination, "%lf ",temps_cpu_liste);
     //liberer_reseau(test_reseau);
-
+    printf("LC fait\n");
     //Table de hachage
     for (taille_tab = 50; taille_tab <= 501; taille_tab+=50){
         temps_initial = clock();
@@ -48,10 +48,11 @@ int main(int argc, char** argv){
         temps_final = clock();
         temps_cpu_hachage = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
         fprintf(destination, "%lf ", temps_cpu_hachage);
-        liberer_reseau(test_reseau);
+        //liberer_reseau(test_reseau);
+        printf("table %d fait\n", taille_tab);
     }
     fprintf(destination,"\n");
-    liberer_chaine(chaines);
+    //liberer_chaine(chaines);
     fclose(source);
     fclose(destination);
 
