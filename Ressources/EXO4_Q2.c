112dffdff3cffc02336bb7a2a3769e37c74cd23b
FlyFird <92992243+FlyFird@users.noreply.github.com>
2024-05-02 01:55:13 +0200

    Update EXO4_Q2.c

--- Ressources/EXO4_Q2.c
+++ Ressources/EXO4_Q2.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int f(int x, int y){
+
+int f(int x, int y) {
     return y + (x + y)*(x + y + 1)/2;
 } 
 
-/*Affichage de la fonction f*/
-int main(){
+/* Affichage de la fonction f */
+int main() {
 
-    for (int i=1; i<11; i++){
-        for (int j=1; j<11; j++){
+    for (int i=1; i<11; i++) {
+        for (int j=1; j<11; j++) {
             printf("%d\n", f(i, j));
         }
     }
     
+    return 0;
 }
